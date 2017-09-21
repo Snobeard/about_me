8f0c09d1a458fe70455b2a56168d77bf8b1482ab
Snobeard <fatmattmatt@gmail.com>
2017-09-20 18:08:56 -0700

    added question 6 (number guessing)

--- app.js
+++ app.js
@@ -1,71 +1,90 @@
 'use strict'; /* helps with declaration of variables and errors */
 
 var score = 0;
-var a1, a2, a3, a4, a5;
-var q1, q2, q3, q4, q5;
+var numberGuess = 4;
+var a1, a2, a3, a4, a5, a6, a7;
+var q1, q2, q3, q4, q5, q6, q7;
+var myStates = ['california', 'georgia', 'new york', 'iowa', 'texas', 'colorado', 'kentucky', 'new mexico', 'michigan'];
 
-var userName = prompt('Greetings! What is your name?');
-alert('Well hello there ' + userName + '! Pleasure to meet you! See if you can get these couple answers right about me.');
+// var userName = prompt('Greetings! What is your name?');
+// alert('Well hello there ' + userName + '! Pleasure to meet you! See if you can get these couple answers right about me.');
+//
+// // Question 1
+// var answer1 = prompt((q1 = 'Do I ride a motorcycle?')).toLowerCase();
+//
+// if (answer1 === 'y' || answer1 === 'yes') {
+//   alert('You are Correct!');
+//   score += 1;
+//   a1 = 'Correct';
+// } else {
+//   alert('You are Incorrect.');
+//   a1 = 'Incorrect';
+// }
+//
+// // Question 2
+// var answer2 = prompt((q2 = 'Have I swam with sharks before?')).toLowerCase();
+//
+// if (answer2 === 'n' || answer2 === 'no') {
+//   alert('Correctamundo!');
+//   score += 1;
+//   a2 = 'Correct';
+// } else {
+//   alert('That is Incorrect.');
+//   a2 = 'Incorrect';
+// }
+//
+// // Question 3
+// var answer3 = prompt((q3 = 'Have I been to Australia?')).toLowerCase();
+//
+// if (answer3 === 'n' || answer3 === 'no') {
+//   alert('Correct! I have not. But I always have wanted to go to Australia.');
+//   score += 1;
+//   a3 = 'Correct';
+// } else {
+//   alert('Incorrect, I have never been to Australia.');
+//   a3 = 'Incorrect';
+// }
+//
+// // Question 4
+// var answer4 = prompt((q4 = 'Can I do a handstand?')).toLowerCase();
+//
+// if (answer4 === 'y' || answer4 === 'yes') {
+//   alert('Correct! I can do a handstand quite well.');
+//   score += 1;
+//   a4 = 'Correct';
+// } else {
+//   alert('That is Incorrect, I actually am capable of walking around on my hands.');
+//   a4 = 'Incorrect';
+// }
+//
+// // Question 5
+// var answer5 = prompt((q5 = 'Are Mini Cooper\'s amazing?')).toLowerCase();
+//
+// if (answer5 === 'y' || answer5 === 'yes') {
+//   alert('Hell yes they are!');
+//   score += 1;
+//   a5 = 'Correct';
+// } else {
+//   alert('You should probably re-think your interests, that is Incorrect.');
+//   a5 = 'Incorrect';
+// }
 
-// Question 1
-var answer1 = prompt((q1 = 'Do I ride a motorcycle?')).toLowerCase();
+// Question 6
+do {
+  var answer6 = prompt(q6 = 'How many licks does it take to get to the center of a tootsie pop?\n (you have ' + numberGuess + ' tries left)');
+  answer6 = Number(answer6);
+  numberGuess -= 1;
+  if (answer6 === 7) {
+    alert('Well Done! It takes 7 licks to get to the center of a tootsie pop!');
+    score += 1;
+  } else if (answer6 < 7) {
+    alert('Too Low!');
+  } else
+    alert('Too High!');
+} while (answer6 !== 7 && numberGuess > 0);
 
-if (answer1 === 'y' || answer1 === 'yes') {
-  alert('You are Correct!');
-  score += 1;
-  a1 = 'Correct';
-} else {
-  alert('You are Incorrect.');
-  a1 = 'Incorrect';
-}
+// Question 7
 
-// Question 2
-var answer2 = prompt((q2 = 'Have I swam with sharks before?')).toLowerCase();
-
-if (answer2 === 'n' || answer2 === 'no') {
-  alert('Correctamundo!');
-  score += 1;
-  a2 = 'Correct';
-} else {
-  alert('That is Incorrect.');
-  a2 = 'Incorrect';
-}
-
-// Question 3
-var answer3 = prompt((q3 = 'Have I been to Australia?')).toLowerCase();
-
-if (answer3 === 'n' || answer3 === 'no') {
-  alert('Correct! I have not. But I always have wanted to go to Australia.');
-  score += 1;
-  a3 = 'Correct';
-} else {
-  alert('Incorrect, I have never been to Australia.');
-  a3 = 'Incorrect';
-}
-
-// Question 4
-var answer4 = prompt((q4 = 'Can I do a handstand?')).toLowerCase();
-
-if (answer4 === 'y' || answer4 === 'yes') {
-  alert('Correct! I can do a handstand quite well.');
-  score += 1;
-  a4 = 'Correct';
-} else {
-  alert('That is Incorrect, I actually am capable of walking around on my hands.');
-  a4 = 'Incorrect';
-}
-
-// Question 5
-var answer5 = prompt((q5 = 'Are Mini Cooper\'s amazing?')).toLowerCase();
-
-if (answer5 === 'y' || answer5 === 'yes') {
-  alert('Hell yes they are!');
-  score += 1;
-  a5 = 'Correct';
-} else {
-  alert('You should probably re-think your interests, that is Incorrect.');
-  a5 = 'Incorrect';
-}
 
 // alert with final score
 if (score > 3) {
@@ -73,7 +92,7 @@ if (score > 3) {
 } else if (score === 3) {
   alert('Not bad, you got ' + score + ' out of 5 correct answers.');
 } else {
-  alert('You should get to know me better, unfortunately you only got ' + score + ' out of 5 correct answers.');
+  alert('You should get to know me better, unfortunately you only got ' + score + ' out of 6 correct answers.');
 }
 
 console.log('User Name: ' + userName);
@@ -82,3 +101,4 @@ console.log('Question 2_' + q2 + ': ' + answer2 + ' was ' + a2);
 console.log('Question 3_' + q3 + ': ' + answer3 + ' was ' + a3);
 console.log('Question 4_' + q4 + ': ' + answer4 + ' was ' + a4);
 console.log('Question 5_' + q5 + ': ' + answer5 + ' was ' + a5);
+console.log('Question 6_' + q6 + ': ' + answer6 + ' was ' + a6);
